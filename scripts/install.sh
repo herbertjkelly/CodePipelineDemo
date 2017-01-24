86c6030b853507814a7bf3dee03b870e44b11363
Herbert <herbertkelly@gmail.com>
2017-01-24 14:06:59 +0200

    v1.4

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
+cd /home/ec2-user/bin
+wget https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh 
+sh ./install.sh
+. /home/ec2-user/.nvm/nvm.sh
 nvm install 4.4.5
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080
--- scripts/runweb.sh
+++ scripts/runweb.sh
@@ -1,1 +1,1 @@
-node /var/app/web/test/js
+/home/ec2-user/.nvm/versions/node/v4.4.5/bin/node /var/app/web/test/js
