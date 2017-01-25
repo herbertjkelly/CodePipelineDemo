b862897e8c5ba2a6b7340cc93ab8ff5b496ceaa2
Herbert <herbertkelly@gmail.com>
2017-01-25 07:27:21 +0200

    v1.10

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@ wget https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh
 sh ./install.sh
 . /home/ec2-user/.nvm/nvm.sh
 nvm install 4.4.5
-iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080
+sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080
