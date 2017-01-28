0065bca919eb09ecc37b9124666bbaa436ea69d3
Herbert <herbertkelly@gmail.com>
2017-01-28 12:57:41 +0200

    V 1.1.0

--- web/test.js
+++ web/test.js
@@ -1,5 +1,6 @@
-require("http").createServer(function(request, response){
-              response.writeHeader(200, {"Content-Type": "text/plain"});
-                response.write("Hello World!");
-                  response.end();
-}).listen(8080);
+ require("http").createServer(function(request, response){
+               response.writeHeader(200, {"Content-Type": "text/plain"});
+                 response.write("Hello World!");
++		response.write("AWS CodePipeline");
+                   response.end();
+ }).listen(8080);
