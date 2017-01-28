db1394591e47649abc10f6be3cca2c0f3ffb9b2b
Herbert <herbertkelly@gmail.com>
2017-01-28 13:05:40 +0200

    V 1.1.0

--- scripts/runweb.sh
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-#!/bin/bash
-/root/.nvm/versions/node/v4.4.5/bin/node  /var/app/web/test.js & 
-exit
-=======
-/root/.nvm/versions/node/v4.4.5/bin/node  /var/app/web/test.js 
->>>>>>> parent of dc862fd... V1.1
