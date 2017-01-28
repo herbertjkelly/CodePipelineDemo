2302f245f0addd72d056d18d3ad8989b0e91dada
Herbert <herbertkelly@gmail.com>
2017-01-28 12:58:58 +0200

    v1.0.1

--- web/test.js
+++ web/test.js
@@ -1,6 +1,5 @@
  require("http").createServer(function(request, response){
                response.writeHeader(200, {"Content-Type": "text/plain"});
                  response.write("Hello World!");
-+		response.write("AWS CodePipeline");
                    response.end();
  }).listen(8080);
